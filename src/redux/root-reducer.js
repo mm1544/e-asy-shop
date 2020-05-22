a21cac381e21685c3e3407ac90719e235d6b0b7c
M. Minskis <martynas.minskis@gmail.com>
2020-05-22 18:05:27 +0100

    refactoring shop-data and directory-dayta to be stored in reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
-import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer';
 import { persistReducer } from 'redux-persist';
 // Importing a type of storage that it is needed. LocalStorage in this case (sessionStorage could be used instead).
 import storage from 'redux-persist/lib/storage';
+import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 // JSON obj. that represents possible configurations for redux-persist
 const persistConfig = {
@@ -16,6 +18,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 // Exporting a modified version of a rootReducer with a persistConfig
